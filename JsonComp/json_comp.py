e7a51d03aa3dbd97bb6441b289d93be1d22bced3
Amit Prakash <2amitprakash@gmail.com>
2019-01-21 22:13:54 -0500

    Basic Compare function done.

--- JsonComp/json_comp.py
+++ JsonComp/json_comp.py
@@ -1,16 +1,88 @@
 import json
 from pprint import pprint
-from tokenize import tokenize
+from readJsonCompConfig import readConfig
 
 #load the JSON file
 def loadJson(filename):
     with open(filename) as f:
         return json.load(f)
+#End of function
 
+#Compare the elements - list or dictionary or anything else
+def compare_json_data(source_data_a,source_data_b):
+    def compare(data_a,data_b):
+        # type: list
+        if (type(data_a) is list):
+            # is [data_b] a list and of same length as [data_a]?
+            if (
+                (type(data_b) != list) or
+                (len(data_a) != len(data_b))
+            ):
+                return False
+            else:
+                # Sort the lists
+                data_a.sort()
+                data_b.sort()
+                # iterate over list items
+                for list_index,list_item in enumerate(data_a):
+                    # compare [data_a] list item against [data_b] at index
+                    if (not compare(list_item,data_b[list_index])):
+                        return False
+                # list identical
+                return True
+        # type: dictionary
+        elif (type(data_a) is dict):
+            # is [data_b] a dictionary?
+            if (type(data_b) != dict):
+                return False
+            # iterate over dictionary keys
+            for dict_key,dict_value in data_a.items():
+                # key exists in [data_b] dictionary, and same value?
+                if (
+                    (dict_key not in data_b) or
+                    (not compare(dict_value,data_b[dict_key]))
+                ):
+                    return False
+            # dictionary identical
+            return True
+        # simple value - compare both value and type for equality
+        else:
+            return data_a == data_b
+    # compare a to b in recursion unless meet the base condition
+    return compare(source_data_a,source_data_b)
+#End of compare
+
+# Compare the 2 list based on configuration
+def compareConfigBased(lhsList, rhsList, lhsFile, rhsFile):
+    if ((len(lhsList) == 1) and (len(rhsList) == 1)):
+        return compare_json_data(lhsFile[lhsList[0]],rhsFile[rhsList[0]])
+    for l in range(len(lhsList)-1):
+        for r in range(len(rhsList)-1):
+            return False            
+
+#End of Function
+
+# Test the Function        
 if __name__=="__main__":
-        file = 'a.json'
-        input_data = loadJson(file)
-        pprint (input_data)
-        var1="carrot"
-        list = input_data[var1]
-        print ("Value is: {v}, and length is {l}".format(v=input_data[var1][0]["fourth"],l=len(list)))
+     #Get first file loaded
+     file = 'a.json'
+     in_data1 = loadJson(file)
+     #pprint (input_data)
+     #Get second file loaded
+     file = 'b.json'
+     in_data2 = loadJson(file)
+     #Get configuration fle loaded
+     list = readConfig("Config.txt")
+     #Compare
+     for m in range(len(list)-1):
+             elemList = list[m]
+             lhsList = elemList[0]
+             rhsList = elemList[1]
+             if (compareConfigBased (lhsList, rhsList, in_data1, in_data2)):
+                print ("Good: Values {l} and {r} matched".format(l=lhsList,r=rhsList))
+             else:   
+                print ("Error: Values {l} and {r} not matched".format(l=lhsList,r=rhsList))
+     #hardcoded example -- can be removed once working code established
+     var1="carrot"
+     list = in_data1[var1]
+     print ("Value is: {v}, and length is {l}".format(v=in_data1[var1][0]["fourth"],l=len(list)))
--- JsonComp/readJsonCompConfig.py
+++ JsonComp/readJsonCompConfig.py
@@ -1,10 +1,15 @@
-from tokenize import tokenize
-
 #read config file and follow the grammer
 #var1.var2 means var2 is nested in a list var1
-def readconfig(filename):
-    with open(filename) as f:
+def readConfig(filename):
+        configlist = [] # the main list with all confg elements
+        with open(filename) as f:
             for line in f:
-                        
-        return load(f)
-
+                list = line.strip('\n').split('=')
+                # a two element list for each element in config list
+                configlist.append([list[0].split('.'), list[1].split('.')])
+        return configlist
+#End of Function        
+#Test the function
+if __name__=="__main__":
+        config = "config.txt"
+        print (readConfig(config))
