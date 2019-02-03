343c050257f69cacc399a03b3d71b5c1eaafd20d
Amit Prakash <2amitprakash@gmail.com>
2019-02-02 22:51:43 -0500

    Cleaned the code and added support for more test cases.

--- JsonComp/json_comp.py
+++ JsonComp/json_comp.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import copy
 from readJsonCompConfig import readConfig
@@ -40,6 +41,7 @@ def compare_json_data(source_data_a,source_data_b):
     def compare(data_a,data_b):
         # type: list
         if (isinstance(data_a, list)):
+            #print("Comparing lists: {a} and {b}".format(a=data_a, b=data_b))
             # is [data_b] a list and of same length as [data_a]?
             if (
                 not (isinstance(data_b, list)) or
@@ -59,6 +61,7 @@ def compare_json_data(source_data_a,source_data_b):
                 return True
         # type: dictionary
         elif (type(data_a) is dict):
+            #print("Comparing dicts: {a} and {b}".format(a=data_a, b=data_b))
             # is [data_b] a dictionary?
             if (type(data_b) != dict):
                 return False
@@ -74,7 +77,7 @@ def compare_json_data(source_data_a,source_data_b):
             return True
         # simple value - compare both value and type for equality
         else:
-            print("Comparing value", data_a, data_b)
+            #print("Comparing values: {a} and {b}".format(a=data_a, b=data_b))
             return data_a == data_b
     # compare b to a in recursion unless meet the base condition
     return compare(source_data_b,source_data_a)
@@ -85,7 +88,7 @@ def _getValueFromJsonFile(lst, lhs):
     #Check if a constant value to return then not read in JSON file
     if ( not (lst == []) and len(lst) == 3):
         if (lst[0] == "R"): #To handle comparison to constant/real values
-            print ("Found R and value ", lst[2])
+            #print ("Found R and value ", lst[2])
             if (lst[1] == "None"):
                 return [None]
             elif (lst[1] == "Int"):
@@ -99,72 +102,81 @@ def _getValueFromJsonFile(lst, lhs):
             else:
                 return [lst[2]]
     #If not constant then
-    if (lhs):
-        file_data = in_data1
+    if (lhs == "lhs"):
         list_lCount = lCount_1
         flat_json = flat_json_1
     else:
-        file_data = in_data2
         list_lCount = lCount_2
         flat_json = flat_json_2
-    #If length == 1 then return directly
-    if (len(lst) == 1):
-        return [file_data[lst[0]]]
-    else:
-        vlist=[]
-        for e_count in range(0,len(lst)):
-            elem = lst[e_count]
-            if (vlist == []):
-                vlist = [elem]
-            else:
-                vlist = [s + elem for s in vlist]
-            #Get the count list for each list type
-            l_count = [list_lCount[v] if v in list(list_lCount) else 0 for v in vlist]
-            # add the elements accordingly to build the list of keys
-            nvlist = []
-            for ct in range(0,len(l_count)):
-                cnt = l_count[ct]
-                if (cnt == 0): # a dict
-                    #print ("dict:", elem)
-                    nvlist.append(vlist[ct] + ".")
-                else: # a list
-                    #print ("list:", elem)
-                    for i in range(0, cnt):
-                        nvlist.append(vlist[ct] + "." + str(i) + ".")
-            vlist = copy.deepcopy(nvlist)
-        return [flat_json[key.strip(".")] for key in vlist]
+    #Start processing the comparison
+    vlist=[]
+    for e_count in range(0,len(lst)):
+        elem = lst[e_count]
+        if (vlist == []):
+            vlist = [elem]
+        else:
+            vlist = [s + elem for s in vlist]
+        #Get the count list for each list type
+        l_count = [list_lCount[v] if v in list(list_lCount) else 0 for v in vlist]
+        # add the elements accordingly to build the list of keys
+        nvlist = []
+        for ct in range(0,len(l_count)):
+            cnt = l_count[ct]
+            if (cnt == 0): # a dict
+                #print ("dict:", elem)
+                nvlist.append(vlist[ct] + ".")
+            else: # a list
+                #print ("list:", elem)
+                for i in range(0, cnt):
+                    nvlist.append(vlist[ct] + "." + str(i) + ".")
+        vlist = copy.deepcopy(nvlist)
+    return [flat_json[key.strip(".")] for key in vlist]
 #End of private function
 
 # Compare the json data elements based on configuration
 def compareConfigBased(elemList):
     if (elemList == []):
         return False
-    value1 = _getValueFromJsonFile(elemList[0],True)
-    value2 = _getValueFromJsonFile(elemList[1],False)
+    value1 = _getValueFromJsonFile(elemList[0],"lhs")
+    value2 = _getValueFromJsonFile(elemList[1],"rhs")
     #print (value1, value2)
     return compare_json_data(value1,value2)
 #End of Function
 
 # Execute the Function        
 if __name__=="__main__":
-     #Get first file loaded
-     file = 'C:\\MyWork\\Abacus\\Utility\\Test\\inputclaim.json' 
-     #file = 'a.json'
-     in_data1 = loadJson(file)
-     flat_json_1, lCount_1 = flatten_json(in_data1)
-     #print ("Flattened JSON ----",flat_json_1)
-     #print ("List of the counts ----", lCount_1)
-     #print (lCount_1["carrot"])
-     #Get second file loaded
-     file = 'C:\\MyWork\\Abacus\\Utility\\Test\\entities_claim.json' 
-     #file = 'b.json'
-     in_data2 = loadJson(file)
-     flat_json_2, lCount_2 = flatten_json(in_data2)
-     #Get configuration fle loaded
-     lst = readConfig("C:\\MyWork\\Abacus\\Utility\\Test\\config.txt")
-     #lst = readConfig("Config.txt")
-     #Compare
-     for m in range(0,len(lst)):
+    file1=""
+    if (len(sys.argv) > 1):
+        if (sys.argv[1] == "test"):
+            file1 = "a.json"
+            file2 = "b.json"
+            cfile = "Config.txt"
+        else:
+            print ("Ignoring arguments. The good parameter to use is test (no quotes). Using defaults.")
+            file1 = "a.json"
+            file2 = "b.json"
+            cfile = "Config.txt"
+    else:
+        #Todo: Ask user to input the 3 file paths
+        print ("User input files...")
+        ############# HARDCODE THE USER DEFINED FILE PATHS HERE ##################
+        #file1 = 'C:\\MyWork\\Abacus\\Utility\\Test\\inputclaim.json' 
+        #file2 = 'C:\\MyWork\\Abacus\\Utility\\Test\\entities_claim.json'
+        #cfile = 'C:\\MyWork\\Abacus\\Utility\\Test\\config.txt'
+        print ("############################################################################################################")
+        print ("Till the functionality to accept user input is implemented, please hardcode the filepath in this code block.")
+        if ( file1 == ""):
+            exit()
+    #Get first file loaded
+    flat_json_1, lCount_1 = flatten_json(loadJson(file1))
+    #print ("Flattened JSON ----",flat_json_1)
+    #print ("List of the counts ----", lCount_1)
+    #Get second file loaded
+    flat_json_2, lCount_2 = flatten_json(loadJson(file2))
+    #Get configuration fle loaded
+    lst = readConfig(cfile)
+    #Compare
+    for m in range(0,len(lst)):
         if (compareConfigBased (lst[m])):
             print ("Good: Values for {l} and {r} matched".format(l=lst[m][0],r=lst[m][1]))
         else:   
